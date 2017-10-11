35f0e49f103ea1ffe628376beb3e1764efe0af00
bolof2000 <bolofindeolusegun@yahoo.com>
2017-10-11 14:08:01 -0500

    Selenium Framework

---
+++ framework/framework/InvalidUsersNotLogin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+
+namespace framework
+{
+    [TestClass]
+    public class InvalidUserNotLogin
+    {
+        [TestMethod]
+        public void RunTest()
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://127.0.0.1:8000/accounts/login/");
+            var user = driver.FindElementById("id_username");
+            user.SendKeys("wrongUsername");
+
+            var pass = driver.FindElementById("id_password");
+            pass.SendKeys("wrongPassword");
+
+            driver.FindElementByXPath("html/body/div[1]/div/form/input[2]").Click();
+
+
+
+        }
+    }
+}
---
+++ framework/framework/ValidUserCanSuccessfullyLogin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+
+namespace framework
+{
+    [TestClass]
+    public class ValidUserCanSuccessfullyLogin
+    {
+        [TestMethod]
+        public void RunTest()
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://127.0.0.1:8000/accounts/login/");
+            var user = driver.FindElementById("id_username");
+            user.SendKeys("bolof2000");
+
+            var pass = driver.FindElementById("id_password");
+            pass.SendKeys("Dammy2k100");
+
+            driver.FindElementByXPath("html/body/div[1]/div/form/input[2]").Click();
+
+            var error = driver.FindElementsByXPath("html/body/div[1]/div/h1");
+            Assert.IsTrue(error.,"not successful")
+         
+           
+
+
+
+           
+
+        }
+    }
+}
